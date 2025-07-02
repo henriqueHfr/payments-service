7c6957c525f941199632f18489a1bbc227fbd843
Henrique <fernandes4626@gmail.com>
2025-07-02 00:44:25 -0300

    init: create project

--- src/main/java/com/hpg/service/payments/repository/AccountUserRepository.java
+++ src/main/java/com/hpg/service/payments/repository/AccountUserRepository.java
@@ -5,16 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountUserRepository extends JpaRepository<AccountUserModel, Long> {
 
-    // Custom query to find user by email
     AccountUserModel findByUserEmail(String userEmail);
 
-    // Custom query to find user by phone
     AccountUserModel findByUserPhone(String userPhone);
 
-    // Custom query to find user by Pix key
     AccountUserModel findByUserPixKey(String userPixKey);
 
-    // Custom query to find user by Pix key type
     AccountUserModel findByUserPixKeyType(String userPixKeyType);
 
     AccountUserModel findByUserValueBalance(Double userValueBalance);
